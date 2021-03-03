8c1d5142d3293fa878fe978d56d0c915928de1bd
Javier <62314170+jarru28@users.noreply.github.com>
2021-03-03 18:33:07 +0100

    Add files via upload

---
+++ bot_nuevo.py
@@ -0,0 +1,81 @@
+import websocket, json, pprint, yagmail
+import pandas as pd
+import talib
+from binance.client import Client
+from binance.enums import *
+import numpy as np
+
+
+
+TRADE_QUANTITY = 20.0 #En moneda base
+NUM_VELAS=10
+bought=False
+close=0
+precio_compra=0.0
+quantity_token=0.0 #Cantidad en primer token
+token_real=0.0 #quantity_token rounded a 6 decimales
+SYMBOL="BTCBUSD"
+SOCKET="wss://stream.binance.com:9443/ws/btcbusd@kline_1m"
+client = Client("xj8fRUF7rI9BSnICoEYbmBr3Kwf3bxwZuIGpj9vOp474tyiad2aufXCCXR0gEQHn","cwxZiytEXXLEdVmPrx7c08ChcZVzVihK9oC3noVhjtHxKurjCnKdlNKubT0YQHme", tld='com')
+
+
+def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
+    try:
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        print(order)
+        correo=yagmail.SMTP("rullet170600@gmail.com","Binjavjai2021")
+        correo.send(to="rullet170600@gmail.com",subject=side,contents=order)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return True
+
+def on_open(ws):
+    print('opened connection')
+
+def on_close(ws):
+    print('closed connection')
+
+def on_message(ws, message): 
+    global bought,close,precio_compra,quantity_token,token_real
+
+    # Almacenar datos cada vez que llegue un mensaje
+    json_message = json.loads(message)
+  
+    candle = json_message['k']
+    is_candle_closed = candle['x']
+    time = candle['t']
+    close = float(candle['c'])
+
+    # Obtener últimos 500 datos
+    candelas= client.get_klines(symbol='BTCBUSD', interval= Client.KLINE_INTERVAL_1MINUTE)
+    pf=pd.DataFrame(candelas)
+    pf=pf.tail(NUM_VELAS)
+    column = pf.iloc[:,4]
+
+    # Calcular indicador
+    upperband, middleband, lowerband = talib.BBANDS(column, timeperiod=10, nbdevup=2, nbdevdn=2)
+    lower=float(lowerband[499])
+    uper=float(upperband[499])
+
+    # Condicion para vender
+    if uper<=close:
+        if close>precio_compra*1.003:
+            if bought==True:
+                order_succeeded = order(SIDE_SELL,token_real,SYMBOL)
+                if order_succeeded:
+                    bought = False
+    # Condicion para comprar
+    if lower>=close:
+        if bought==False:
+            precio_compra=close
+            quantity_token=TRADE_QUANTITY/close
+            token_real=round(quantity_token,6)
+            order_succeeded = order(SIDE_BUY,token_real,SYMBOL)
+            if order_succeeded:
+                bought = True
+                    
+ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+
+ws.run_forever()
